832b477a6bc18bc272abe2f08ef64d8478c17e01
GleciaGaba <gleciasf@hotmail.com>
2024-09-10 11:38:58 +0200

    add et remove update

--- movie.py
+++ movie.py
@@ -1,8 +1,9 @@
 import os
+import json
+import logging
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
-print(DATA_FILE)
 
 
 class Movie:
@@ -13,12 +14,34 @@ class Movie:
         return self.title
 
     def _get_movies(self):
-        pass
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
 
-    def _write_movies(self):
-        pass
+    def _write_movies(self, movies):
+        with open(DATA_FILE, "w") as f:
+            json.dump(movies, f, indent=4)
+
+    def add_movies(self):
+        movies = self._get_movies()
+        if self.title not in movies:
+            movies.append(self.title)
+            self._write_movies(movies)
+            return True
+        else:
+            logging.warning(f"Le film {self.title} est déjà enregistré.")
+            return False
+
+    def remove_movies(self):
+        movies = self._get_movies()
+        if self.title in movies:
+            movies.remove(self.title)
+            self._write_movies(movies)
+            return True
+        else:
+            logging.warning(f"Le filme {self.title} n'est pas dans la liste.")
+            return False
 
 
 if __name__ == '__main__':
-    m = Movie("harry potter")
-    print(m)
+    m = Movie("Cube")
+    m.remove_movies()
